a9c32beff4dbdb5c0ad52d02b789ac2686ebcb09
Saishree-07 <155431896+Saishree-07@users.noreply.github.com>
2024-11-29 09:46:13 +0530

    Delete E-commerce Shopping Cart System directory

--- E-commerce Shopping Cart System/Ecommerceoutput.sql
+++
@@ -1,8 +0,0 @@
-USE ecommerce_db;
-SELECT * FROM Users;
-SELECT * FROM Products;
-SELECT * FROM DiscountCodes;
-SELECT * FROM CartItems;
-SELECT * FROM Orders;
-SELECT * FROM OrderItems;
-SELECT * FROM Payments;
